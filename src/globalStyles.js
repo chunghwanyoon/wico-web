207bb3ad68fe66d6007b2ef230e17ba889bf040b
chunghwanyoon <chunghwan@redrocket.co.kr>
2021-01-22 17:49:31 +0900

    add empty global styles using styled-components

---
+++ src/globalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+  /* global font sizes */
+  .title {}
+  .subtitle {}
+  .description {}
+`;
+
+export default GlobalStyle;
